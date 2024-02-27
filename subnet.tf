fed12836c0077eb8176d785f6566fc8585899f35
Vivek Mrudhuvan B <126495994+chintuu77@users.noreply.github.com>
2024-02-27 23:54:56 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -4,7 +4,7 @@ resource "aws_subnet" "public-subnet1" {
   vpc_id                  = aws_vpc.vivek.id
   cidr_block              = var.subnet1_cidr
   map_public_ip_on_launch = "true"
-  availability_zone       = "eu-north-1b"
+  availability_zone       = "us-east-2c"
 
   tags = {
     Name = "public-subnet1"
