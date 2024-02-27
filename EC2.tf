201b7a41cd920d1076251fc609d7bbbca912220f
Vivek Mrudhuvan B <126495994+chintuu77@users.noreply.github.com>
2024-02-27 23:53:19 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "wordpress8" {
-  ami                         = "ami-0208423bef18c17d1"
-  instance_type               = "t3.micro"
-  key_name                    = "JN"
+  ami                         = "ami-0c7f9161f8491665f"
+  instance_type               = "t2.micro"
+  key_name                    = "WP"
   vpc_security_group_ids      = [aws_security_group.web-sg.id]
   subnet_id                   = aws_subnet.public-subnet1.id
   associate_public_ip_address = true
