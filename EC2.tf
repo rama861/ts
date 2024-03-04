7e2957402c6b7f7e9677003c23f166e60ecbe96e
Vivek Mrudhuvan B <126495994+chintuu77@users.noreply.github.com>
2024-03-04 21:07:03 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,12 +1,12 @@
-resource "aws_instance" "wordpress8" {
+resource "aws_instance" "fish" {
   ami                         = "ami-0c7f9161f8491665f"
   instance_type               = "t2.micro"
   key_name                    = "WP"
   vpc_security_group_ids      = [aws_security_group.web-sg.id]
   subnet_id                   = aws_subnet.public-subnet1.id
   associate_public_ip_address = true
-  user_data                   = file("data.sh")
+  user_data                   = file("data1.sh")
   tags = {
-    name = "wordpress"
+    name = "fish-app"
   }
 }
