73d0fdfcd7a2ac473dc98cfd4f29d95c36f20368
Vivek Mrudhuvan B <126495994+chintuu77@users.noreply.github.com>
2024-03-05 22:56:02 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,12 +1,12 @@
-resource "aws_instance" "fish" {
-  ami                         = "ami-0c7f9161f8491665f"
-  instance_type               = "t2.micro"
-  key_name                    = "WP"
+resource "aws_instance" "Penguin" {
+  ami                         = "ami-0208423bef18c17d1"
+  instance_type               = "t3.micro"
+  key_name                    = "JN"
   vpc_security_group_ids      = [aws_security_group.web-sg.id]
   subnet_id                   = aws_subnet.public-subnet1.id
   associate_public_ip_address = true
   user_data                   = file("data1.sh")
   tags = {
-    name = "fish-app"
+    name = "Penguin-app"
   }
 }
