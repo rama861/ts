3503e49de3fc993e9b4ac4817ff2162ebfe88963
rama861 <161824892+rama861@users.noreply.github.com>
2024-03-19 10:09:00 -0500

    Update data1.sh

--- data1.sh
+++ data1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum -y install git
-git clone https://github.com/chintuu77/Penguin.git
+git clone https://github.com/rama861/Penguin.git
 cd Penguin/
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
